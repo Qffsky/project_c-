9ed5ede291c0138f7a6d0e0fbccde713b128708f
Qffsky <step4kap@gmail.com>
2024-09-11 22:07:56 +0300

    Hack.

--- constexpr_func.cpp
+++ constexpr_func.cpp
@@ -2,15 +2,14 @@
 #include <iostream>
 
 
-//https://stackoverflow.com/questions/59909712/how-to-compute-the-determinant-of-a-matrix-in-c-using-recursive-functions
 template <int N>
 constexpr std::array<std::array<int, N - 1>, N - 1> FillSubMatrix(const std::array<std::array<int, N>, N>& matrix, int column) {
     std::array<std::array<int, N - 1>, N - 1> subMatrix{};
-    int a = 0, b = 0;
-    for (int i = 1; i < N; ++i) {
-        for (int j = 0; j < N; ++j) {
+    unsigned long a = 0, b = 0;
+    for (unsigned long i = 1; i < N; ++i) {
+        for (unsigned long j = 0; j < N; ++j) {
 	    if (j != column) {
-	        subMatrix[a][b] = matrix[i][j];
+		    (&std::get<0>((&std::get<0>(subMatrix))[a]))[b] = (&std::get<0>((&std::get<0>(matrix))[i]))[j];
 		b++;
 	    }
 	}
@@ -20,15 +19,17 @@ constexpr std::array<std::array<int, N - 1>, N - 1> FillSubMatrix(const std::arr
     return subMatrix;
 }
 
+
 template <int N>
 constexpr int Det(const std::array<std::array<int, N>, N>& matrix) {
     if constexpr (N == 1) {
-        return matrix[0][0];
+        return std::get<0>(std::get<0>(matrix));
     } else {
-        int determinant = 0, i = 0;
+        int determinant = 0;
+	unsigned long i = 0;
        	while (i < N) {
 	    std::array<std::array<int, N - 1>, N - 1> subMatrix = FillSubMatrix<N>(matrix, i); 
-            determinant += matrix[0][i] * (i & 1 ? -1 : 1) * Det<N - 1>(subMatrix);
+            determinant += (&std::get<0>(std::get<0>(matrix)))[i] * (i & 1 ? -1 : 1) * Det<N - 1>(subMatrix);
 	    ++i;
     	}
 
